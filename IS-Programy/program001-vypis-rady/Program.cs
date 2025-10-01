283a9a422a6ff19fd7e0e0d74a6f64ef4594fc9d
Patrik Hečko <patrik.hecko@tul.cz>
2025-10-01 10:11:39 +0200

    první program  #3

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,40 @@
+﻿ConsoleColor color = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+
+string again = "a";
+
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("");
+    Console.WriteLine("###############################################################");
+    Console.WriteLine("##### Program vypise radu cisel od 1 do zadaneho cisla n. #####");
+    Console.WriteLine("###############################################################");
+    Console.WriteLine("#####                    Patrik Hečko                     #####");
+    Console.WriteLine("###############################################################");
+    Console.WriteLine("");
+    
+        Console.Write("Zadej první číslo: ");
+        int first = int.Parse(Console.ReadLine() ?? "0");
+
+        Console.Write("Zadej poslední číslo: ");
+        int last = int.Parse(Console.ReadLine() ?? "0");
+
+        Console.Write("Zadej krok: ");
+        int step = int.Parse(Console.ReadLine() ?? "1");
+
+        if (step <= 0)
+        {
+            Console.WriteLine("! Krok musí být větší než 0 !");
+            return;
+        }
+
+        // Výpis řady čísel
+        for (int i = first; i <= last; i += step)
+        {
+            Console.WriteLine(i);
+        }
+
+    Console.Write("Zadejte 'a' pro opakovani: ");
+    again = Console.ReadLine() ?? "";
+}   
