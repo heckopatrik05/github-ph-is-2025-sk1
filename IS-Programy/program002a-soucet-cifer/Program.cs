3bc02805774ce8e664276b6e7f9a22c1aa007e6e
Patrik Hečko <patrik.hecko@tul.cz>
2025-10-15 10:02:13 +0200

    #5 zalozeni programu 002

---
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,27 @@
+﻿ConsoleColor color = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("");
+    Console.WriteLine("##########################################");
+    Console.WriteLine("##### Ciferný součet číslic a součin #####");
+    Console.WriteLine("##########################################");
+    Console.WriteLine("#####           Patrik Hečko         #####");
+    Console.WriteLine("##########################################");
+    Console.WriteLine("");
+
+    Console.Write("Zadejte číslo: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Nezadali jste celé číslo! Zadejte číslo znovu: ");
+    }
+    
+    //Opakovani programu
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
