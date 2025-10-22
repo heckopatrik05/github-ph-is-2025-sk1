62f8e20535867b13191bb668b5cc51b4e5adfa61
Patrik Hečko <patrik.hecko@tul.cz>
2025-10-22 10:25:22 +0200

    Obdelnik HOTOVO #7

--- IS-Programy/program004a-obdelnik/Program.cs
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ConsoleColor color = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+
+string again = "a";
+while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("#################################");
+            Console.WriteLine("##### Vykresleni obdelniku  #####");
+            Console.WriteLine("#################################");
+            Console.WriteLine("#####     Patrik Hečko      #####");
+            Console.WriteLine("#################################");
+            Console.WriteLine("");
+
+            // Načtení stran obdélníku
+            int a, b;
+
+            Console.Write("Zadejte délku strany a (šířka): ");
+            
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+                Console.Write("Zadejte celé kladné číslo pro stranu a: ");
+
+            Console.Write("Zadejte délku strany b (výška): ");
+            
+            while (!int.TryParse(Console.ReadLine(), out b) || b <= 0)
+                Console.Write("Zadejte celé kladné číslo pro stranu b: ");
+
+            Console.WriteLine();
+
+            // Vykreslení obdélníku
+            for (int i = 0; i < b; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+          
+            //###########################################################################################
+            // Opakování programu
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+            again = Console.ReadLine();
+        }      
