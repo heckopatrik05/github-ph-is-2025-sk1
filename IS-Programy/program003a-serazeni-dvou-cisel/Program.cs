661c4c05c2bea11c7ff62094a65e1829c7e1127c
Patrik Hečko <patrik.hecko@tul.cz>
2025-10-22 09:37:30 +0200

    hotove 003 #6

---
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -0,0 +1,52 @@
+﻿ConsoleColor color = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("");
+    Console.WriteLine("#############################");
+    Console.WriteLine("##### Řazení dvou čísel #####");
+    Console.WriteLine("#############################");
+    Console.WriteLine("#####   Patrik Hečko    #####");
+    Console.WriteLine("#############################");
+    Console.WriteLine("");
+
+    Console.Write("Zadejte číslo - hodnota A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo! Zadejte znovu hodnotu A: ");
+    }
+
+    Console.Write("Zadejte číslo - hodnota B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo! Zadejte znovu hodnotu B: ");
+    }
+
+    // pokud je číslo záporné, uděláme z něj kladné
+    if (a < 0)
+        a = -a;
+
+    if (b < 0)
+        b = -b;
+    //###########################################################################################
+    // Řazení čísel
+
+    int pom;
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+    }
+    Console.WriteLine($"Seřazená čísla jsou: {a}, {b}");
+    
+    // Opakování programu
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
---
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -0,0 +1,60 @@
+﻿ConsoleColor color = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+
+string again = "a";
+while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("#################################");
+            Console.WriteLine("##### Největší ze tří čísel #####");
+            Console.WriteLine("#################################");
+            Console.WriteLine("#####     Patrik Hečko      #####");
+            Console.WriteLine("#################################");
+            Console.WriteLine("");
+
+            // Načtení čísel
+            int a, b, c;
+
+            Console.Write("Zadejte číslo - hodnota A: ");
+            while (!int.TryParse(Console.ReadLine(), out a))
+                Console.Write("Nezadali jste celé číslo! Zadejte znovu hodnotu A: ");
+
+            Console.Write("Zadejte číslo - hodnota B: ");
+            while (!int.TryParse(Console.ReadLine(), out b))
+                Console.Write("Nezadali jste celé číslo! Zadejte znovu hodnotu B: ");
+
+            Console.Write("Zadejte číslo - hodnota C: ");
+            while (!int.TryParse(Console.ReadLine(), out c))
+                Console.Write("Nezadali jste celé číslo! Zadejte znovu hodnotu C: ");
+
+            // Pokud je číslo záporné, uděláme z něj kladné
+            if (a < 0) a = -a;
+            if (b < 0) b = -b;
+            if (c < 0) c = -c;
+
+            //###########################################################################################
+            // Algoritmus podle vývojového diagramu – nalezení největšího čísla
+
+            Console.WriteLine();
+            if (a > b)
+            {
+                if (a > c)
+                    Console.WriteLine($"Největší číslo je: {a}");
+                else
+                    Console.WriteLine($"Největší číslo je: {c}");
+            }
+            else
+            {
+                if (b > c)
+                    Console.WriteLine($"Největší číslo je: {b}");
+                else
+                    Console.WriteLine($"Největší číslo je: {c}");
+            }
+
+            //###########################################################################################
+            // Opakování programu
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+            again = Console.ReadLine();
+        }      
