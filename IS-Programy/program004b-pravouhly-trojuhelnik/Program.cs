638bcee47feec8586574a960b08bea8c69e1f1b3
Patrik Hečko <patrik.hecko@tul.cz>
2025-10-23 14:45:01 +0200

    trojuhelnik #7

--- IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
+++ IS-Programy/program004b-pravouhly-trojuhelnik/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("#################################");
+            Console.WriteLine("##### Vykreslení trojúhelníku #####");
+            Console.WriteLine("#################################");
+            Console.WriteLine("#####       Patrik Hečko       #####");
+            Console.WriteLine("#################################");
+            Console.WriteLine("");
+
+            int vyska;
+
+            Console.Write("Zadejte výšku trojúhelníku: ");
+
+            while (!int.TryParse(Console.ReadLine(), out vyska) || vyska <= 0)
+                Console.Write("Zadejte celé kladné číslo pro výšku: ");
+
+            Console.WriteLine();
+
+            // Vykreslení pravouhlého trojúhelníku
+            for (int i = 1; i <= vyska; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            //###########################################################################################
+            // Opakování programu
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+            again = Console.ReadLine();
+        }
