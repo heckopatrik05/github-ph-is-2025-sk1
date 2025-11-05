fefc929dc81999db9dcd3642916116506afbc4b1
Patrik Hečko <patrik.hecko@tul.cz>
2025-11-05 09:19:14 +0100

    zalozeni a vlozeni zakladniho kodu

---
+++ IS-Programy/program005-generator/Program.cs
@@ -0,0 +1,27 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Generátor čísel *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Patrik Hečko *******");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
