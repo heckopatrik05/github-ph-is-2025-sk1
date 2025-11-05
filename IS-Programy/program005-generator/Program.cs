eb63cc6850904c8c02271ad85dd28afe1c7d62be
Patrik Hečko <patrik.hecko@tul.cz>
2025-11-05 09:49:41 +0100

    generovani nahodnych cisel Úkol  #8

--- IS-Programy/program005-generator/Program.cs
+++ IS-Programy/program005-generator/Program.cs
@@ -1,23 +1,60 @@
-﻿string again = "a";
+﻿ConsoleColor originalColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.Green;
+
+string again = "a";
 while (again == "a")
 {
     Console.Clear();
     Console.WriteLine("****************************");
-    Console.WriteLine("***** Generátor čísel *****");
+    Console.WriteLine("***** Generátor čísel  *****");
     Console.WriteLine("****************************");
     Console.WriteLine("******* Patrik Hečko *******");
     Console.WriteLine("****************************");
     Console.WriteLine();
 
-    Console.Write("Zadejte první číslo řady (celé číslo): ");
-    int first;
-
-    while (!int.TryParse(Console.ReadLine(), out first))
+    Console.Write("Zadejte kolik bude čísel v řadě (celé číslo): ");
+    int n;
+    while (!int.TryParse(Console.ReadLine(), out n))
     {
         Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
     }
 
+    Console.Write("Zadejte dolni mez řady (celé číslo): ");
+    int low;
+    while (!int.TryParse(Console.ReadLine(), out low))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez řady znovu: ");
+    }
 
+    Console.Write("Zadejte horní mez řady (celé číslo): ");
+    int top;
+    while (!int.TryParse(Console.ReadLine(), out top))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte horní mez řady znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("############################");
+    Console.WriteLine("kolik bude čísel v řadě: " + n);
+    Console.WriteLine("dolní mez: " + low);
+    Console.WriteLine("horní mez: " + top);
+    Console.WriteLine("############################");
+    Console.WriteLine();
+
+    // Definice pole
+    int[] myRandNumbs = new int[n];
+
+    Random myRandNumb = new Random();
+
+    Console.WriteLine();
+    Console.WriteLine("############################");
+    Console.WriteLine("Pseudonáhodná čísla: ");
+
+    for (int i = 0; i < n; i++)
+    {
+        myRandNumbs[i] = myRandNumb.Next(low, top + 1);
+        Console.Write(myRandNumbs[i] + ", ");
+    }
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
