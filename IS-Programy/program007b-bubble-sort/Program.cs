28e693d2ceefce646d248597b5d540310c3292c8
Patrik Hečko <patrik.hecko@tul.cz>
2025-11-19 10:14:28 +0100

    hotovo kod i s obrazcem #10

--- IS-Programy/program007b-bubble-sort/Program.cs
+++ IS-Programy/program007b-bubble-sort/Program.cs
@@ -1,4 +1,5 @@
-﻿ConsoleColor originalColor = Console.ForegroundColor;
+﻿using System.Diagnostics;
+ConsoleColor originalColor = Console.ForegroundColor;
 Console.ForegroundColor = ConsoleColor.Green;
 
 string again = "a";
@@ -43,7 +44,6 @@ while (again == "a")
 
     // Definice pole
     int[] myRandNumbs = new int[n];
-
     Random myRandNumb = new Random();
 
     Console.WriteLine();
@@ -54,6 +54,88 @@ while (again == "a")
     {
         myRandNumbs[i] = myRandNumb.Next(low, top + 1);
         Console.Write(myRandNumbs[i] + ", ");
+    }
+
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    int change = 0;
+    int compare = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            compare++;
+            if (myRandNumbs[j] < myRandNumbs[j + 1])
+            {
+                int temp = myRandNumbs[j + 1];
+                myRandNumbs[j + 1] = myRandNumbs[j];
+                myRandNumbs[j] = temp;
+                change++;
+            }
+        }
+    }
+    stopwatch.Stop();
+
+    Console.WriteLine();
+    Console.WriteLine("Seřazená čísla: ");
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write(myRandNumbs[i] + ", ");
+    }
+    Console.WriteLine();
+    Console.WriteLine("Počet změn: " + change);
+    Console.WriteLine("Počet porovnání: " + compare);
+    Console.WriteLine("Čas běhu programu: " + stopwatch.ElapsedMilliseconds + " ms");
+
+    int secondMax = 0;
+    bool foundSecond = false;
+
+    if (n >= 2)
+    {
+        int maxVal = myRandNumbs[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (myRandNumbs[i] < maxVal)
+            {
+                secondMax = myRandNumbs[i];
+                foundSecond = true;
+                break;
+            }
+        }
+    }
+
+    Console.WriteLine();
+    if (foundSecond)
+    {
+        Console.WriteLine("Druhé největší číslo je " + secondMax + ".");
+        Console.WriteLine("Obrazec:");
+        
+        for (int r = 0; r < secondMax; r++)
+        {
+            if (r == 0 || r == 1 || r == secondMax - 1 || r == secondMax - 2)
+            {
+                for (int c = 0; c < secondMax; c++)
+                {
+                    Console.Write("*");
+                }
+            }
+            else
+            {
+                Console.Write("*");
+                for (int c = 0; c < secondMax - 2; c++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+    else
+    {
+        Console.WriteLine("Nelze určit druhé největší číslo (čísla jsou stejná nebo je jich málo).");
     }
 
     Console.WriteLine();
