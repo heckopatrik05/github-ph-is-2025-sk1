f80a653e83064019e0b1b3b3e3f188dad1b05cf1
Patrik Hečko <patrik.hecko@tul.cz>
2025-12-03 10:27:47 +0100

    neco hotove #15

---
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -0,0 +1,53 @@
+﻿string again = "a";
+while (again == "a")
+{
+   
+    Console.Clear();
+
+    razitko();
+
+    ulong a = nactiCislo("Zadejte první číslo a: ");
+    ulong b = nactiCislo("Zadejte druhé číslo b: ");
+
+    
+    static void razitko()
+    {
+        Console.WriteLine("***********************************");
+        Console.WriteLine("***** Výpočet čisla NSN a NSD *****");
+        Console.WriteLine("***********************************");
+        Console.WriteLine("********** Patrik Hečko ***********");
+        Console.WriteLine("***********************************");
+    }
+
+    static ulong nactiCislo( string zprava )
+    {
+        Console.Write(zprava);
+        ulong cislo;
+
+        while (!ulong.TryParse(Console.ReadLine(), out cislo))
+        {
+            Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+        }
+        
+        return cislo;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
